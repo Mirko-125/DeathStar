f53edee98828803161da7cc9e0e0e0d43b6a5341
Mirko <mirkoat21@gmail.com>
2023-05-07 00:27:25 +0200

    Mapiranja Pt. 2

--- Entiteti/GradoviPlanete.cs
+++ Entiteti/GradoviPlanete.cs
@@ -10,5 +10,27 @@ namespace DeathStar.Entiteti
     {
         public virtual Planeta GradPlaneta { get; set; }
         public virtual string Grad { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+
+            var other = (GradoviPlanete)obj;
+
+            return GradPlaneta == other.GradPlaneta && Grad == other.Grad;
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hash = 17;
+                hash = hash * 23 + GradPlaneta.GetHashCode();
+                hash = hash * 23 + Grad.GetHashCode();
+                return hash;
+            }
+        }
     }
 }
